c496be146bd15454d2136f4dbf9fb67de4179956
Preston Guillory <pguillory@gmail.com>
2013-02-18 22:40:15 -0800

    Smart aiming

--- app/js/Game/Units/Base.js
+++ app/js/Game/Units/Base.js
@@ -75,14 +75,63 @@ Base.prototype.move = function(terrain) {
   this.reticle.velocity = this.reticle.target.minus(this.origin).times(0.18)
 
   this.timeToFire -= 1
-  if (this.reticle && this.player.firedThisTurn && this.timeToFire <= 0 && this.player.gold > 0) {
-    // SOUNDS.shoot()
-    this.player.deductGold(BASE_SHOT_VALUE)
-    this.emitFire(this.origin, this.reticle.velocity.wiggle(1.0))
-    this.timeToFire = 9
+  if (this.player.firedThisTurn && this.timeToFire <= 0 && this.player.gold > 0) {
+    this.fire()
   }
 
   return true
+}
+
+Base.prototype.fire = function() {
+  // SOUNDS.shoot()
+  this.player.deductGold(BASE_SHOT_VALUE)
+  console.log('origin: ' + this.origin)
+  var d = this.reticle.target.minus(this.origin)
+  console.log('d: ' + d)
+
+  var speed = 4
+
+  var v = new Vector(1, Math.abs(d.x / 2) - speed / 2)
+
+  if (d.x < 0) {
+    v.x *= -speed
+  } else {
+    v.x *= speed
+  }
+  v.y /= -speed
+
+  // console.log('initial v: ' + v)
+
+  if (d.y < 0) {
+    var granularity = 0.5
+    var gain = d.y
+    var launch = v.y
+    var impact = v.y
+    for (var i = 0; i < 50; i++) {
+      // console.log('gain: ' + gain, 'launch: ' + launch, 'impact: ' + impact)
+      impact += granularity
+      launch -= granularity
+      if (impact > 0) {
+        console.log('too high, can not reach')
+        break
+      }
+      gain -= launch * granularity
+      gain -= impact * granularity
+      if (gain > 0) {
+        console.log('got it')
+        break
+      }
+    }
+    v.y = launch + 0.75 * granularity
+    // console.log('adjusted v: ' + v)
+  }
+
+  // v.y += 1
+
+  this.emitFire(this.origin, v/*.wiggle(1.0)*/)
+
+  // this.emitFire(this.origin, this.reticle.velocity.wiggle(1.0))
+  this.timeToFire = 1
 }
 
 Base.prototype.draw = function(canvas) {
@@ -90,8 +139,8 @@ Base.prototype.draw = function(canvas) {
 
   var color = this.player.color
   this.position.tap(function(x, y) {
-    for (var dy = -3; dy <= 0; dy += 3) {
-      for (var dx = -3; dx <= 3; dx += 3) {
+    for (var dy = -3; dy <= 0; dy += 1) {
+      for (var dx = -3; dx <= 3; dx += 1) {
         canvas.setPixel(x + dx, y + dy, color)
       }
     }
